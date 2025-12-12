b14d0f3be07adc0311ebf0bd9d9ccb98d4428595
Swarup Shekhar <swarupshekhar@Swarups-MacBook-Air.local>
2025-12-12 14:58:42 +0530

    restore Jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { EmailModule } from '../email/email.module.js';
-import { PrismaModule } from '../prisma/prisma.module.js'; // Assuming PrismaModule import is needed
+import { PrismaModule } from '../prisma/prisma.module.js';
+import { JwtStrategy } from './jwt.strategy.js';
 
 @Module({
   imports: [
     PrismaModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secret',
       signOptions: { expiresIn: '1d' },
@@ -15,7 +18,7 @@ import { PrismaModule } from '../prisma/prisma.module.js'; // Assuming PrismaMod
     EmailModule
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService]
 })
 export class AuthModule { }
