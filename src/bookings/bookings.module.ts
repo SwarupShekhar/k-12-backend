c0aa8d4d68f44c9211516ea89d3417053893d541
Swarup Shekhar <swarupshekhar@Swarups-MacBook-Air.local>
2025-12-12 19:03:40 +0530

    fix missing email module dependency

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -1,11 +1,11 @@
-// src/bookings/bookings.module.ts
 import { Module } from '@nestjs/common';
 import { BookingsService } from './bookings.service.js';
 import { BookingsController } from './bookings.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { EmailModule } from '../email/email.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EmailModule],
   controllers: [BookingsController],
   providers: [BookingsService],
   exports: [BookingsService],
